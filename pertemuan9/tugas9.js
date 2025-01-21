d0219048492d35b99c6c2e7111ae811734c3fa39
fqihhh <arsyad2104@student.abudzar.sch.id>
2025-01-21 13:42:30 +0700

    tugas9

---
+++ pertemuan9/tugas9.js
@@ -0,0 +1,29 @@
+//soal1
+let nilaimtk = 91;
+let nilaiipa = 81;
+let nilaiips = 91;
+
+if (nilaimtk >= 91 && nilaiipa >= 91 && nilaiips >= 91){
+    console.log('lulus');
+} else {
+    console.log('tdk lulus');
+}
+
+//soal2
+let nilaiMtk = 91;
+let nilaiIpa = 81;
+let nilaiIps = 91;
+
+if(nilaiMtk >= 91 || nilaiIpa >= 91 || nilaiIps >= 91){
+  console.log('andi lulus')
+}else{
+  console.log('tidak lulus')
+}
+//soal3
+let isLogin = false;
+if(!isLogin){
+  console.log('anda belum login')
+
+}else{
+  console.log('anda sudah login')
+}
