48b5eaeff5831c02bba2460441d9baa4f5806b27
fqihhh <arsyad2104@student.abudzar.sch.id>
2025-01-28 11:12:14 +0700

    tugas11

---
+++ pertemuan11-loop/script.js
@@ -0,0 +1,27 @@
+//no1
+let angka = 1; 
+
+while (angka <= 10) { 
+    console.log(angka); 
+    angka++; 
+}
+//no2
+let a = 10; 
+
+do {
+    console.log(a); 
+    a--; 
+} while (a >= 1);
+//no3
+for (let n = 2; n <= 20; n += 2) { 
+    console.log(n); 
+}
+//no4
+for (let numbr = 1; numbr <= 10; numbr++) { 
+    if (numbr === 5) { 
+        continue; 
+    }
+    console.log(numbr); 
+}
+//no5
+belum
