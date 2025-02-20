58cb14eef7990c3c6112ccfaa3dbb56aca577cda
fqihhh <arsyad2104@student.abudzar.sch.id>
2025-02-20 12:05:05 +0700

    tugas17

---
+++ pertemuan17/script1.js
@@ -0,0 +1,31 @@
+"strict mode"
+let umur = 25; 
+console.log(umur);
+console.log(umur);  
+
+
+x = 10;
+console.log(x); 
+
+
+let data = "Halo";
+console.log(typeof data); 
+
+data = 42;
+console.log(typeof data);
+
+data = true;
+console.log(typeof data);
+
+
+let name = prompt("masukkan nama anda")
+alert("hello" + name + "selamat datang di pondok");
+
+
+let result = confirm("apakah pengguna ingin melanjutkan ke halaman lain?")
+console.log(result)
+if(result == true){
+    alert("anda memilih untuk melanjutkan") 
+}else{
+    alert("Anda membatalkan tindakan")
+}
